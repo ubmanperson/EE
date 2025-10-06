e2f6453732457bf1cf89d73b821af76f3145bcdc
BruhMomentum106 <uuganbayar.bts@gmail.com>
2025-10-06 14:46:44 +0800

    first commit

---
+++ .ipynb_checkpoints/training-checkpoint.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+# Fix color code for blue (missing #)
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+plt.figure()
+plt.scatter(X[:, 2], X[:, 3], c=y, cmap=cmap, edgecolor='k', s=20)
+plt.show()  # Add parentheses to actually display the plot
